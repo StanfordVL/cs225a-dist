be7f45b6e5c2e86a0db901bfc06f0bdf20435c3d
Vinay Sriram <vsriram25@gmail.com>
2018-11-04 04:23:58 -0800

    made changes to makefile

--- make.sh
+++ make.sh
@@ -19,42 +19,39 @@ cd ..
 # Insert helper scripts into bin directory
 cd bin
 
-# Jaco Controller Script
+# Mico Controller + Simulator Script
 if [ -f 'controller' ]; then
-    cat <<EOF > init_jaco.sh
-redis-cli flushall
-redis-cli set cs225a::robot::jaco::tasks::kp_pos 300
-redis-cli set cs225a::robot::jaco::tasks::kv_pos 40
-redis-cli set cs225a::robot::jaco::tasks::kp_ori 300
-redis-cli set cs225a::robot::jaco::tasks::kv_ori 30
-redis-cli set cs225a::robot::jaco::tasks::kp_joint 0
-redis-cli set cs225a::robot::jaco::tasks::kv_joint 30
-redis-cli set cs225a::robot::jaco::tasks::kp_joint_init 300
-redis-cli set cs225a::robot::jaco::tasks::kv_joint_init 10
-redis-cli set cs225a::robot::jaco::tasks::jt_pos_des "4.71238898038 1.57079632679 1.57079632679 0.0 0.0 0.0"
-./run_controller.sh controller resources/controller/world_jaco.urdf resources/controller/jaco.urdf jaco
-EOF
-fi
-
-# Mico Controller Script
-if [ -f 'controller' ]; then
-    cat <<EOF > init_mico.sh
-redis-cli flushall
-redis-cli set cs225a::robot::mico::tasks::kp_pos 300
-redis-cli set cs225a::robot::mico::tasks::kv_pos 40
-redis-cli set cs225a::robot::mico::tasks::kp_ori 300
-redis-cli set cs225a::robot::mico::tasks::kv_ori 30
-redis-cli set cs225a::robot::mico::tasks::kp_joint 0
-redis-cli set cs225a::robot::mico::tasks::kv_joint 30
-redis-cli set cs225a::robot::mico::tasks::kp_joint_init 300
-redis-cli set cs225a::robot::mico::tasks::kv_joint_init 10
-redis-cli set cs225a::robot::mico::tasks::jt_pos_des "1.48481 3.22206 4.90119 2.04572 4.81604 1.88954"
-redis-cli set cs225a::robot::mico::tasks::jt_pos_des "4.71238898038 1.57079632679 1.57079632679 0.0 0.0 0.0" 
+    cat <<EOF > init_controller.sh
+sh set_gains.sh mico
 ./run_controller.sh controller resources/controller/world_mico.urdf resources/controller/mico.urdf mico
 EOF
 fi
 
-# Run Generic Controller Script
+# Mico Standalone Controller Script
+if [ -f 'controller' ]; then
+    cat <<EOF > init_standalone.sh
+sh set_gains.sh mico
+./run_standalone.sh controller resources/controller/world_mico.urdf resources/controller/mico.urdf mico
+EOF
+fi
+
+# Set Gains Script
+if [ -f 'controller' ]; then
+    cat <<EOF > set_gains.sh
+redis-cli flushall
+redis-cli set cs225a::robot::\$1::tasks::kp_pos 5
+redis-cli set cs225a::robot::\$1::tasks::kv_pos 2
+redis-cli set cs225a::robot::\$1::tasks::kp_ori 5
+redis-cli set cs225a::robot::\$1::tasks::kv_ori 2
+redis-cli set cs225a::robot::\$1::tasks::kp_joint 5
+redis-cli set cs225a::robot::\$1::tasks::kv_joint 2
+redis-cli set cs225a::robot::\$1::tasks::kp_joint_init 5
+redis-cli set cs225a::robot::\$1::tasks::kv_joint_init 2
+redis-cli set cs225a::robot::\$1::tasks::jt_pos_des "4.71238898038 1.57079632679 1.57079632679 0.0 0.0 0.0"
+EOF
+fi
+
+# Run Generic Controller + Simulator Script
 if [ -f 'controller' ]; then
 	cat <<EOF > run_controller.sh
 if [ "\$#" -lt 4 ]; then
@@ -71,6 +68,25 @@ else
 fi
 EOF
 	chmod +x run_controller.sh
+fi
+
+# Run Generic Standalone Controller (No Simulator) Script
+if [ -f 'controller' ]; then
+	cat <<EOF > run_standalone.sh
+if [ "\$#" -lt 4 ]; then
+	cat <<EOM
+This script calls ./visualizer and the specified controller simultaneously.
+All the arguments after the controller will be passed directly to it.
+
+Usage: sh run.sh <controller-executable> <path-to-world.urdf> <path-to-robot.urdf> <robot-name> <extra_controller_args>
+EOM
+else
+	trap 'kill %1; kill %2' SIGINT
+	trap 'kill %1; kill %2' EXIT
+	./visualizer \$2 \$3 \$4 > visualizer.log & ./"\$@"
+fi
+EOF
+	chmod +x run_standalone.sh
 fi
 
 cd ..
